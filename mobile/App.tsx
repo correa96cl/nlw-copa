a301c6db9569105ac99ef3faaf582735ae979e5b
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2022-12-24 22:55:18 -0300

    Commiteo de novas funcionalidades mobile e web

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,23 +6,25 @@ import { Loading } from './src/components/Loading';
 import React from "react";
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { SignIn } from './src/screens/Signin';
+import { AuthContextProvider } from './src/contexts/AuthContext';
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar barStyle='light-content'
-        backgroundColor="transparent" 
-        translucent/>
+      <AuthContextProvider>
+        <StatusBar barStyle='light-content'
+          backgroundColor="transparent"
+          translucent />
 
 
 
-      {
-        fontsLoaded ? <SignIn /> : <Loading />
-      }
+        {
+          fontsLoaded ? <SignIn /> : <Loading />
+        }
 
-
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
