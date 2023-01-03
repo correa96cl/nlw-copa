e9b951a70c0d51141d1f824bd1c331b8b9a99501
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2023-01-02 22:02:56 -0300

    segundo commiteo do ano

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -59,7 +59,7 @@ export async function poolRoutes(fastify: FastifyInstance) {
 
     )
 
-    fastify.post('/pools/:id/join',
+    fastify.post('/pools/join',
         { onRequest: [authenticate] },
         async (request, reply) => {
             const joinPoolBody = z.object({
@@ -112,4 +112,88 @@ export async function poolRoutes(fastify: FastifyInstance) {
 
             return reply.status(201).send();
         });
+
+    fastify.get('/pools', {
+        onRequest: [authenticate]
+    }, async (request) => {
+        const pools = await prisma.pool.findMany({
+            where: {
+                participants: {
+                    some: {
+                        userId: request.user.sub
+                    }
+                }
+            },
+            include: {
+                _count: {
+                    select: {
+                        participants: true,
+                    }
+                },
+                participants: {
+                    select: {
+                        id: true,
+                        user: {
+                            select: {
+                                avatarUrl: true
+                            }
+                        }
+                    },
+                    take: 4,
+                },
+                owner: {
+                    select: {
+                        id: true,
+                        name: true,
+                    }
+                }
+            }
+        })
+
+        return { pools }
+    })
+
+    fastify.get('/pools/:id', {
+        onRequest: [authenticate],
+    }, async (request) => {
+
+        const getPoolParams = z.object({
+            id: z.string(),
+        })
+
+        const { id } = getPoolParams.parse(request.params);
+
+        const pools = await prisma.pool.findUnique({
+            where: {
+                id,
+            },
+            include: {
+                _count: {
+                    select: {
+                        participants: true,
+                    }
+                },
+                participants: {
+                    select: {
+                        id: true,
+                        user: {
+                            select: {
+                                avatarUrl: true
+                            }
+                        }
+                    },
+                    take: 4,
+                },
+                owner: {
+                    select: {
+                        id: true,
+                        name: true,
+                    }
+                }
+            }
+        });
+
+        return { pools }
+
+    })
 }
